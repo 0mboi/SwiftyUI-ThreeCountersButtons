ea6231a5dfc6469df50c4425d6ca5e705f4efdcd
Christopher Hicks <visrule@protonmail.com>
2021-01-28 12:01:36 -0800

    Initial Commit

---
+++ ThreeCounters/ThreeCountersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeCountersApp.swift
+//  ThreeCounters
+//
+//  Created by Christopher Hicks on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ThreeCountersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
